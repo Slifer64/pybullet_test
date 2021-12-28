d9069e75f177256751d936fd134e842765042b60
Slifer64 <antosidi64@yahoo.gr>
2021-12-28 17:23:13 +0200

    playing with grid world

--- scripts/small_grid_world.py
+++ scripts/small_grid_world.py
@@ -1,31 +1,55 @@
 import numpy as np
+import enum
 
-def policyIteration(world, gamma, tol_stop, max_iter):
 
-    V = np.zeros((world.m,world.n))
-    V_new = np.zeros((world.m,world.n))
+class Policy (enum.Enum):
 
-    iter = 1
+    MAX = 1
+    RANDOM = 2
 
-    while True:
+
+# ============= Policy Evaluation ==============
+
+def policyEvaluation(world, policy, gamma, zero_tol=1e-3, max_iter=100):
+
+    assert isinstance(policy, Policy)
+
+    V = np.zeros(world.size())
+    V_new = np.zeros(world.size())
+
+    iter = 0
+
+    n_act = len(world.Actions)
+
+    while iter < max_iter:
         for s in world.States:
-            v_temp = 0
-            for a,p in zip(world.Actions,world.act_prob):
+            q = np.zeros(n_act)
+            for i,a in enumerate(world.Actions):
                 R, s_new = world.step(s, a)
-                v_temp += p*( R + gamma*V[s_new[0],s_new[1]] )
-            V_new[s[0],s[1]] = v_temp
+                q[i] = R + gamma*V[s_new[0],s_new[1]]
+            if policy == Policy.MAX:
+                V_new[s[0], s[1]] = np.amax(q)
+            elif policy == Policy.RANDOM:
+                V_new[s[0], s[1]] = np.sum(q) / len(q)
 
-        V_diff = V_new - V
-        V = V_new
+        err = np.amax(np.abs(V_new - V))
+        V = V_new.copy()
 
-        if all( v_diff<tol_stop for v_diff in V_diff ):
+        if err < zero_tol:
             break
 
         iter += 1
-        if iter > max_iter:
-            break
+
+    return V, err, iter
 
 
+# ============= Policy Iteration ==============
+def policyIteration(world, gamma, zero_tol=1e-3, max_iter=100):
+
+
+# ============================================
+# ============= GridWorld class ==============
+# ============================================
 class GridWorld:
 
     def __init__(self, grid_size):
@@ -34,16 +58,20 @@ class GridWorld:
 
         self.States = [ ( int( state_id / self.n) , state_id % self.n) for state_id in range(self.m * self.n) ]
 
-        self.act_prob = [0.25, 0.25, 0.25, 0.25]
         self.Actions = [ "north", "east", "south", "west" ]
-        self.Actions_act = [ (-1, 0), (0, 1), (1, 0), (0, -1) ]
-        self.Actions_dict = { name:value for (name,value) in zip(self.Actions,self.Actions_act) }
+        
+        self.__Actions_act = [ (-1, 0), (0, 1), (1, 0), (0, -1) ] # translate verbal actions to actual actions in the grid world
+        self.__Actions_dict = { name:value for (name,value) in zip(self.Actions,self.__Actions_act) } # map them in a dictionary
 
-        self.s_terminal = [(0,0), (self.m-1, self.n-1)]
+        self.__s_terminal = [(0,0), (self.m-1, self.n-1)]
+
+    def size(self):
+
+        return self.m, self.n
 
     def step(self, state, action):
 
-        act = self.Actions_dict.get(action)
+        act = self.__Actions_dict.get(action)
         if act == None:
             raise RuntimeError("Invalid action '" + action + "'...")
 
@@ -72,7 +100,7 @@ class GridWorld:
 
     def isTerminal(self, state):
 
-        for s_t in self.s_terminal:
+        for s_t in self.__s_terminal:
             if state[0]==s_t[0] and state[1]==s_t[1]:
                 return True
         else:
@@ -84,29 +112,22 @@ class GridWorld:
                 "Actions: " + str(self.Actions)
 
 
+# =================================
+# ============= MAIN ==============
+# =================================
+
 if __name__ == '__main__':
 
     grid_world = GridWorld((4,4))
 
-    s = (3,2)
-    act = "north"
-    R, new_s = grid_world.step(s, act)
+    # s = (3,2)
+    # act = "north"
+    # R, new_s = grid_world.step(s, act)
+    # print("state:", s, ", action:", act, ", new_state:", new_s, ", R:", R)
 
-    print("state:", s, ", action:", act, ", new_state:", new_s, ", R:", R)
+    V, err, iter = policyEvaluation(grid_world, policy=Policy.RANDOM, gamma=1.0, zero_tol=1e-3, max_iter=150)
 
+    print(V)
+    print("Error:",err, ", iterations:",iter)
 
-    A = np.random.rand(2,2)
-    # B = np.random.rand(2,2)
-    B = A + 5e-1
-
-    print(A)
-    print(B)
-
-    if np.any((A-B)>1e-3):
-        print("A != B")
-    else:
-        print("A == B")
-
-    # print(grid_world)
-
-    # print("Hello world!")
+    # print( grid_world._GridWorld__Actions_act )

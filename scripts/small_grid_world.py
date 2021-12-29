d771de0889cf5dbd18a27b4f7a620501f542b3d3
Slifer64 <antosidi64@yahoo.gr>
2021-12-30 00:07:09 +0200

    fsdfsdf

--- scripts/small_grid_world.py
+++ scripts/small_grid_world.py
@@ -4,6 +4,119 @@ import abc
 
 State = (int, int)
 
+# ============================================
+# ============= GridWorld class ==============
+# ============================================
+class GridWorld:
+
+    def __init__(self, grid_size):
+
+        self.m, self.n = grid_size
+
+        self.states = [self.ind2state(ind) for ind in range(self.m * self.n)]
+
+        self.actions = ["north", "east", "south", "west"]
+
+        self.__actions_act = [(-1, 0), (0, 1), (1, 0),
+                              (0, -1)]  # translate verbal actions to actual actions in the grid world
+        self.__actions_dict = {name: value for (name, value) in
+                               zip(self.actions, self.__actions_act)}  # map them in a dictionary
+
+        self.__s_terminal = [(0, 0), (self.m - 1, self.n - 1)]
+
+    def size(self) -> (int, int):
+
+        return self.m, self.n
+
+    def step(self, state: State, action) -> (float, State):
+
+        act = self.__actions_dict.get(action)
+        if act == None:
+            raise RuntimeError("Invalid action '" + action + "'...")
+
+        if self.isTerminal(state):
+            return (0, state)
+
+        i, j = state
+
+        i += act[0]
+        j += act[1]
+
+        if i < 0:
+            i = 0
+        elif i > self.m - 1:
+            i = self.m - 1
+
+        if j < 0:
+            j = 0
+        elif j > self.n - 1:
+            j = self.n - 1
+
+        R = -1  # reward
+        new_state = (i, j)
+
+        return (R, new_state)
+
+    def isTerminal(self, state: State) -> bool:
+
+        for s_t in self.__s_terminal:
+            if state[0] == s_t[0] and state[1] == s_t[1]:
+                return True
+        else:
+            return False
+
+    def state2ind(self, state: State) -> int:
+
+        return state[0] * self.n + state[1]
+
+    def ind2state(self, ind: int) -> State:
+
+        return int(ind / self.n), ind % self.n
+
+    def __str__(self):
+
+        return "States: " + str(self.states) + "\n" \
+                                               "Actions: " + str(self.actions)
+
+
+# ================================================
+# ============= ValueFunction class ==============
+# ================================================
+class ValueFunction:
+
+    def __init__(self, states: [State] = None):
+        self.state_value_dict = {}
+        if states is not None:
+            self.state_value_dict = {state: 0.0 for state in states}
+
+    def get(self, state: State) -> float:
+        value = self.state_value_dict.get(state)
+        if value is None:
+            raise RuntimeError("Invalid state!")
+        return value
+
+    def set(self, state: State, value: float) -> None:
+        if self.state_value_dict.get(state) is None:
+            raise RuntimeError("Invalid state!")
+        self.state_value_dict[state] = value
+
+    def diff(self, other_value_fun) -> float:
+
+        err = 0.
+        for state in self.state_value_dict.keys():
+            temp_err = abs( self.get(state) - other_value_fun.get(state) )
+            if temp_err > err:
+                err = temp_err
+        return err
+
+    def copy(self):
+        return self.__copy__()
+
+    def __copy__(self):
+        cp = ValueFunction()
+        cp.state_value_dict = self.state_value_dict.copy()
+        return cp
+
 
 # =========================================
 # ============= Policy class ==============
@@ -43,52 +156,32 @@ class RandomPolicy(Policy):
         return str(self.__policy)
 
 
-# def greedyPolicy(states, actions, V):
-#
-#     n_states = len(states)
-#     n_act = len(actions)
-#
-#     P = np.zeros((n_states, n_act)) * (1.0 / n_act)
-#
-#     return P
+# ===============================================
+# ============= RandomPolicy class ==============
+# ===============================================
+class GreedyPolicy(Policy):
 
-# ================================================
-# ============= ValueFunction class ==============
-# ================================================
-class ValueFunction:
+    def __init__(self, world, value_fun: ValueFunction):
 
-    def __init__(self, states: [State] = None):
-        self.state_value_dict = {}
-        if states is not None:
-            self.state_value_dict = {state: 0.0 for state in states}
+        n_states = len(world.states)
+        n_act = len(world.actions)
 
-    def get(self, state: State) -> float:
-        value = self.state_value_dict.get(state)
-        if value is None:
-            raise RuntimeError("Invalid state!")
-        return value
+        self.__policy = {}
+        for state in world.states:
+            self.__policy[state] = {}
+            for act in world.actions:
+                _, s_new = world.step(state, act)
+                v = value_fun.get(s_new)
+                self.__policy[state][act] = 0.0
+            
 
-    def set(self, state: State, value: float) -> None:
-        if self.state_value_dict.get(state) is None:
-            raise RuntimeError("Invalid state!")
-        self.state_value_dict[state] = value
+    def get(self, state, action):
 
-    def diff(self, other_value_fun) -> float:
+        return self.__policy[state][action]
 
-        err = 0.
-        for state in self.state_value_dict.keys():
-            temp_err = abs( self.get(state) - other_value_fun.get(state) )
-            if temp_err > err:
-                err = temp_err
-        return err
+    def __str__(self):
 
-    def copy(self):
-        return self.__copy__()
-
-    def __copy__(self):
-        cp = ValueFunction()
-        cp.state_value_dict = self.state_value_dict.copy()
-        return cp
+        return str(self.__policy)
 
 
 def valueFunctionAsMatrix(value_fun: ValueFunction, states: [State]) -> np.array(float):
@@ -134,85 +227,6 @@ def policyEvaluation(world, policy: Policy, gamma, zero_tol=1e-3, max_iter=100) 
         iter += 1
 
     return V, err, iter
-
-
-# ============= Policy Iteration ==============
-# def policyIteration(world, gamma, zero_tol=1e-3, max_iter=100):
-
-
-# ============================================
-# ============= GridWorld class ==============
-# ============================================
-class GridWorld:
-
-    def __init__(self, grid_size):
-
-        self.m, self.n = grid_size
-
-        self.states = [self.ind2state(ind) for ind in range(self.m * self.n)]
-
-        self.actions = ["north", "east", "south", "west"]
-
-        self.__actions_act = [(-1, 0), (0, 1), (1, 0),
-                              (0, -1)]  # translate verbal actions to actual actions in the grid world
-        self.__actions_dict = {name: value for (name, value) in
-                               zip(self.actions, self.__actions_act)}  # map them in a dictionary
-
-        self.__s_terminal = [(0, 0), (self.m - 1, self.n - 1)]
-
-    def size(self):
-
-        return self.m, self.n
-
-    def step(self, state, action):
-
-        act = self.__actions_dict.get(action)
-        if act == None:
-            raise RuntimeError("Invalid action '" + action + "'...")
-
-        if self.isTerminal(state):
-            return (0, state)
-
-        i, j = state
-
-        i += act[0]
-        j += act[1]
-
-        if i < 0:
-            i = 0
-        elif i > self.m - 1:
-            i = self.m - 1
-
-        if j < 0:
-            j = 0
-        elif j > self.n - 1:
-            j = self.n - 1
-
-        R = -1  # reward
-        new_state = (i, j)
-
-        return (R, new_state)
-
-    def isTerminal(self, state):
-
-        for s_t in self.__s_terminal:
-            if state[0] == s_t[0] and state[1] == s_t[1]:
-                return True
-        else:
-            return False
-
-    def state2ind(self, state):
-
-        return state[0] * self.n + state[1]
-
-    def ind2state(self, ind):
-
-        return int(ind / self.n), ind % self.n
-
-    def __str__(self):
-
-        return "States: " + str(self.states) + "\n" \
-                                               "Actions: " + str(self.actions)
 
 
 # =================================

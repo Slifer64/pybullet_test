d8a1f0d575ce8630958a88ddf24dab820974a7d0
Slifer64 <antosidi64@yahoo.gr>
2022-02-02 01:37:11 +0200

    fsdfsdg

--- scripts/pytorch/temp.py
+++ scripts/pytorch/temp.py
@@ -10,3 +10,20 @@ if __name__ == '__main__':
 
     print(type(x))
 
+    a_ = [[i, i**2, i**3] for i in range(10)]
+
+    a = torch.tensor(a_)
+    m, n = a.shape
+
+    ind = torch.tensor([i%n for i in range(m)])
+
+    print(ind)
+    print(ind.shape)
+
+    print(a)
+    print(a.shape)
+
+    b = torch.gather(a, 1, ind.view(-1, 1)).view()
+
+    print(b)
+
